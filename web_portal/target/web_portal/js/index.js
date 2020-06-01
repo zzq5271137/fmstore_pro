92cfd6876758f7907c2e0ca405c75e552ba534d8
ZHENGQING ZHU <zhuzhengqing321@qq.com>
2020-06-01 18:54:44 +0800

    no message

--- web_portal/target/web_portal/js/index.js
+++ web_portal/target/web_portal/js/index.js
@@ -16,6 +16,8 @@ new Vue({
                  * 由于我们是将从服务器获取到的数据放入contentList的下标为1的位置, contentList[1]原本是不存在的,
                  * 相当于我们给contentList动态地加载了一个属性(而不是修改属性的值), 这个新添加的属性是不会更新到页面上的,
                  * 我们需要主动地告诉页面我们添加的属性以及它的值, 页面才会更新; 下面这句代码就是在做这个;
+                 *
+                 * Vue.set(object, key, value)
                  * Vue.set()用法参考: https://blog.csdn.net/qq_30455841/article/details/78666571
                  */
                 Vue.set(_this.contentList, categoryId, _this.contentList[categoryId]);
